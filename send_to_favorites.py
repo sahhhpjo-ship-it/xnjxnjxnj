670211fc5d0b3b910441592b9d269e6f6512e7d4
sahhhpjo-ship-it <sahhhpjo@gmail.com>
2025-08-12 01:22:19 +0500

    Update send_to_favorites.py

--- send_to_favorites.py
+++ send_to_favorites.py
@@ -8,33 +8,40 @@ from command import fox_command  # Импортируем fox_command из comma
 file = __file__  # Используем __file__ для получения имени текущего файла
 basename = os.path.basename(file)
 
-
-@Client.on_message(fox_command("fmsg", "Отправляет текст в избранное", basename) & filters.me)
+# Определение команды fmsg
+@Client.on_message(filters.me & fox_command("fmsg", "Отправляет текст в избранное", basename))
 async def send_to_favorites(client, message):
     """Отправляет указанный текст в Избранное."""
     try:
-        if len(message.command) < 2:
-            await message.edit("<i>Ошибка: Текст сообщения не указан. Используйте: fmsg [текст]</i>")
+        # Получаем текст сообщения
+        text = message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else None
+
+        # Проверяем, что текст указан
+        if not text:
+            await message.edit("Ошибка: Текст сообщения не указан. Используйте: fmsg [текст]")
             return
 
-        text = " ".join(message.command[1:])
-        await message.delete()  # Удаляем сообщение команды
+        # Удаляем сообщение команды
+        await message.delete()
 
+        # Отправляем текст в Избранное
         await client.send_message("me", text)
 
     except Exception as e:
         try:
-            await client.send_message(message.chat.id, f"<i>Ошибка при отправке в избранное:</i> {e}")
+            await client.send_message(message.chat.id, f"Ошибка при отправке в избранное: {e}")
         except:
-            print(f"Произошла ошибка {e}") # Если send_message не сработает, то выводим в консоль
+            print(f"Произошла ошибка {e}")  # Если send_message не сработает, то выводим в консоль
 
 
-@Client.on_message(fox_command("help_fmsg", "Помощь по команде fmsg", basename) & filters.me)
+# Определение команды help_fmsg
+@Client.on_message(filters.me & fox_command("help_fmsg", "Помощь по команде fmsg", basename))
 async def help_fmsg(client, message):
     """Выводит справку по команде fmsg."""
     await message.edit(
-        f"""<b>fmsg</b> - Отправляет текст в избранное.\nИспользование: fmsg [текст]"""
+        f"""fmsg - Отправляет текст в избранное.\nИспользование: fmsg [текст]"""
     )
+
 
 print("Модуль send_to_favorites.py загружен")
 
