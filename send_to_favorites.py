a5492921fbdbdca15be0e2e9bfde442e58ca0cd6
sahhhpjo-ship-it <sahhhpjo@gmail.com>
2025-08-12 01:05:30 +0500

    Update send_to_favorites.py

--- send_to_favorites.py
+++ send_to_favorites.py
@@ -2,33 +2,19 @@ import asyncio
 import os
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler
-
-# Импорт PREFIX из prefix.py (но он нам не понадобится)
-#from prefix import PREFIX
+from command import fox_command  # Импортируем fox_command из command.py
 
 # Получаем имя файла для использования в fox_command
 file = __file__  # Используем __file__ для получения имени текущего файла
 basename = os.path.basename(file)
 
 
-def fox_command(command, description, file_basename, usage=""):
-    """Декоратор для регистрации команд в FoxUserbot БЕЗ ПРЕФИКСА."""
-    def decorator(func):
-        @Client.on_message(filters.command(command, prefixes="") & filters.me)  # prefixes="" - без префикса
-        async def wrapper(client, message):
-            await func(client, message)  # Вызываем функцию обработчика
-        wrapper.__doc__ = f"{description}\nИспользование: `{command} {usage}`"  # Добавляем документацию
-        return wrapper  # Возвращаем функцию
-
-    return decorator
-
-
-@fox_command(command="fmsg", description="Отправляет текст в избранное", file_basename=basename, usage="[текст]")
+@Client.on_message(fox_command("fmsg", "Отправляет текст в избранное", basename, "[текст]") & filters.me)
 async def send_to_favorites(client, message):
     """Отправляет указанный текст в Избранное."""
     try:
         if len(message.command) < 2:
-            await message.edit("<i>Ошибка: Текст сообщения не указан. Используйте: `fmsg [текст]`</i>")
+            await message.edit("<i>Ошибка: Текст сообщения не указан. Используйте: fmsg [текст]</i>")
             return
 
         text = " ".join(message.command[1:])
@@ -43,12 +29,11 @@ async def send_to_favorites(client, message):
             print(f"Произошла ошибка {e}") # Если send_message не сработает, то выводим в консоль
 
 
-@fox_command(command="help_fmsg", description="Помощь по команде fmsg", file_basename=basename)
+@Client.on_message(fox_command("help_fmsg", "Помощь по команде fmsg", basename) & filters.me)
 async def help_fmsg(client, message):
     """Выводит справку по команде fmsg."""
     await message.edit(
-        f"""<b>fmsg</b> - Отправляет текст в избранное.\nИспользование: `fmsg [текст]`"""
+        f"""<b>fmsg</b> - Отправляет текст в избранное.\nИспользование: fmsg [текст]"""
     )
-
 
 
